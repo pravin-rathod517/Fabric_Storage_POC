3652f0092ffb954b9d2cddd95118ea4a116aa24c
pravin-rathod517 <Pravin.Rathod@dynatechconsultancy.com>
2025-09-18 11:32:05 +0530

    Committing 1 item from workspace 290f6b06-3796-4928-9d58-f562fd0bfecd

---
+++ Testing/Notebook 6.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "efb5676a-d439-4d96-a9ea-abc3fd926d45",
+# META       "default_lakehouse_name": "MDM_Bronze_Layer",
+# META       "default_lakehouse_workspace_id": "290f6b06-3796-4928-9d58-f562fd0bfecd",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "9484b134-f256-4f49-b93c-4f25a1223ead"
+# META         },
+# META         {
+# META           "id": "efb5676a-d439-4d96-a9ea-abc3fd926d45"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC Describe History MDM_Bronze_Layer_Old.D365.validation_execution_master
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
