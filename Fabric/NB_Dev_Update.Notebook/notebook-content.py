7e80c8fe24870f85302549094c539fc3c9be77df
pravin-rathod517 <Pravin.Rathod@dynatechconsultancy.com>
2025-10-30 15:58:00 +0530

    Committing 3 items from workspace 45369a67-ed55-4bfc-80f3-dab572c49d66

---
+++ Fabric/NB_Dev_Update.Notebook/notebook-content.py
@@ -0,0 +1,20 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
